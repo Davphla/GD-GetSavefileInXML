d7650bc409fec10eb77b1862ceb96cf1e784faba
david <david.gozlan@epitech.eu>
2023-10-23 14:21:13 +0200

    feat - fix dumb thing

--- crypto.js
+++ crypto.js
@@ -1,27 +1,29 @@
-
+// utf-8 encoding
 function xor(str, key) {
-    str = String(str).split('').map(letter => letter.charCodeAt());
-    let res = "";
-    for (let i = 0; i < str.length; i++) res += String.fromCodePoint(str[i] ^ key);
-    return res;
+    let result = '';
+    for (let i = 0; i < str.length; i++) {
+        result += String.fromCharCode(key ^ str.charCodeAt(i));
+    }
+    return result;
 }
 
 function decode(data) {
     console.log("Decoding...")
     if (data.startsWith('<?xml version="1.0"?>'))
         return data
+    console.log(data)
+    data = xor(data, 11)
+    data = data.replace(/\0/g, "")
+    console.log(data)
+    data = atob(data)
+    data = data.replace(/-/g, '+').replace(/_/g, '/')
+    data = data.replace(/><(?!\/)/g, '> <')
+    console.log(data)
+    data = data.split('').map(function (x) {
+        return x.charCodeAt(0);
+    });
+    data = new Uint8Array(data);
+    data = pako.inflate(data);
+    return new Uint16Array(data).reduce((acc, byte) => acc + String.fromCharCode(byte), '');
 
-    let decoded = xor(data, 11)
-    decoded = atob(decoded)
-    try {
-        let charData = decoded.split('').map(function (x) {
-            return x.charCodeAt(0);
-        });
-        let binData = new Uint8Array(charData);
-        let fulldata = pako.inflate(binData);
-        let strData = String.fromCharCode.apply(null, new Uint16Array(fulldata));
-        return strData
-    } catch (e) {
-        return console.log("Error! GD save file seems to be corrupt!")
-    }
 }
